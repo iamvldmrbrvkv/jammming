392de72d6b60844d85c919a271895590fe0ecbca
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-08-27 16:35:44 +0300

    fixed useEffect cleanup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,7 +25,7 @@ function App() {
         setToken('');
       }, 1000*60*60);
     };
-    return () => window.history.replaceState({}, 'Jammming', '/');
+    return () => window.history.replaceState({}, 'Jammming', 'jammming');
   }, []);
 
   useEffect(() => {
