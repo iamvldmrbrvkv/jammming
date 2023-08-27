4e542474c52d3868f992fc0fb09ab8a95d8251b0
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-08-27 18:15:03 +0300

    fixing window.history.replaceState()

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -61,7 +61,7 @@ function App() {
         setToken('');
       }, 1000*60*60);
     };
-    return () => window.history.replaceState({}, 'Jammming', '/');
+    return () => window.history.replaceState({}, 'Jammming', 'jammming/');
   }, [token]);
 
   useEffect(() => {
